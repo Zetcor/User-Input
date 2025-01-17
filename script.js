781726ac817efd5524f33c6d706ba39274ee3848
Zetcor <vincecholai@gmail.com>
2025-01-17 11:04:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayInput() {
+    const userInput = document.getElementById('userInput').value;
+    const output = document.getElementById('output');
+    output.textContent = `You entered: ${userInput}`;
+  }
+  
